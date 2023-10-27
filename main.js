c76dcbcb498533aade1716c2583118f44d68b0e8
r1h4n <111518762+r1h4n@users.noreply.github.com>
2023-10-27 11:27:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-function tocasom (idElementoAudio) {document.querySelector(idElementoAudio).play();}
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+
+}
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+
+//o que é uma contante...
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+
+let contador = 0;
+
+while  (contador <9) {
+
+    ListaDeTeclas[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    console.log(contador);
+
+}
+
+
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+//document.querySelector('#som_tecla_pom').play();
+//document.querySelector('.tecla_pom')
